75bc3fe1f66a86c3712ee9338bdaba19777918dc
JuanfeRomero <juanferomerocode@gmail.com>
2020-10-28 04:11:41 -0300

    creada la pagina principal, estilo con bootstrap y agregado mapa con leaflet.js

---
+++ public/javascripts/map.js
@@ -0,0 +1,14 @@
+var mymap = L.map('main_map').setView([-34.6012424, -58.3861497], 13);
+
+const accessToken = 'pk.eyJ1IjoianVhbmZlcm9tZXJvIiwiYSI6ImNrZ3QwZWEyZjE1YXQycnIzYWg5YjZ3eDgifQ.UcYj7Ytgvua8nHNX65Jq-w'
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: accessToken
+}).addTo(mymap);
+
+var marker = L.marker([-34.6040424, -58.3815000]).addTo(mymap);

1de36ca1c04675be8dd2707f268604542338b7ab
JuanfeRomero <juanferomerocode@gmail.com>
2020-12-09 21:41:36 -0300

    cambiada ruta en el authRoute

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,7 +6,7 @@ router.get('/google',
   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
   
 
-router.get('/google/callback', 
+router.get('./google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');

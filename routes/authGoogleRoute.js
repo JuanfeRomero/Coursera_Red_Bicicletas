4f0be53b785b099ec98c7e33f491098444069bb8
JuanfeRomero <juanferomerocode@gmail.com>
2020-12-11 12:44:45 -0300

    intentando detectar el mail, si no funcion habrá que cambiar la API que se utiliza

--- routes/authGoogleRoute.js
+++ routes/authGoogleRoute.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const passport = require('../config/passport');
 
 router.get('/',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'] }));
   
 
 router.get('/callback',
